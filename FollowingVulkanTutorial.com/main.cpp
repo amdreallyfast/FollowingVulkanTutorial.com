a300c10018095771593aceb4516e4664f9a60021
amdreallyfast <amdreallyfast@hotmail.com>
2018-10-27 22:39:05 -0700

    Updated to Vulkan 1.1.85.0. Still experimenting with callback info. Not destroying things before the instance still doesn't seem to be a problem.

--- FollowingVulkanTutorial.com/main.cpp
+++ FollowingVulkanTutorial.com/main.cpp
@@ -331,7 +331,7 @@ private:
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_MAKE_VERSION(1, 1, 82);
+        appInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
 
         // have to ask GLFW for info about the required extensions
         uint32_t glfwExtensionCount = 0;
@@ -375,8 +375,12 @@ private:
         instanceCreateInfo.pApplicationInfo = &appInfo;
         instanceCreateInfo.enabledExtensionCount = static_cast<uint32_t>(requiredExtensions.size());
         instanceCreateInfo.ppEnabledExtensionNames = requiredExtensions.data();
-        instanceCreateInfo.enabledLayerCount = static_cast<uint32_t>(requiredValidationLayers.size());
-        instanceCreateInfo.ppEnabledLayerNames = requiredValidationLayers.data();
+
+        if (mEnableValidationLayers) {
+            instanceCreateInfo.enabledLayerCount = static_cast<uint32_t>(requiredValidationLayers.size());
+            instanceCreateInfo.ppEnabledLayerNames = requiredValidationLayers.data();
+        }
+
         instanceCreateInfo.pNext = &debugCreateInfo;
 
         if (vkCreateInstance(&instanceCreateInfo, nullptr, &mInstance) != VK_SUCCESS) {
